27a6ac0541738ebd2badf74ded1fb88f89b4ffee
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-05-26 15:07:40 +0100

    working on datasets folder

---
+++ datasets/download-double-pendulum.sh
@@ -0,0 +1,3 @@
+wget https://dax-cdn.cdn.appdomain.cloud/dax-double-pendulum-chaotic/2.0.1/double-pendulum-chaotic.tar.gz -P data
+mkdir -p data/double-pendulum
+tar -xvzf data/double-pendulum-chaotic.tar.gz -C data/double-pendulum
