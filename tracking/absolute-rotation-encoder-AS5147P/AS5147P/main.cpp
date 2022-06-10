e599248f394ad4e25c7047574c0d17ff3b50d487
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-06-10 12:46:35 +0100

    first version of encoder driver

---
+++ tracking/absolute-rotation-encoder-AS5147P/AS5147P/main.cpp
@@ -0,0 +1,64 @@
+// pins
+
+#define PIN_CSN 10
+#define PIN_SCK 13
+#define PIN_MISO 12
+#define PIN_MOSI 11
+#define ENCODER_BUFFER 16
+#define ENCODER_BUFFER_MAX_INDEX 15
+#define ENCODER_BUFFER_VALUE_MASK       (0x3FFF)
+
+void as5147p_setup()
+{
+    pinMode(PIN_CSN, OUTPUT);
+    pinMode(PIN_MOSI, OUTPUT);
+    pinMode(PIN_MISO, INPUT);
+    pinMode(PIN_SCK, OUTPUT);
+    digitalWrite(PIN_MOSI, HIGH); // This can be left high as the command is 0xFFFF (all ones)
+}
+
+boolean as5147p_get_raw_sensor_value(uint16_t &value)
+{
+    // set value to zero
+    value = 0;
+    // define parity and bit read buffer
+    uint8_t slave_read_bit, parity = 0;
+
+    // for each data frame
+
+    // set CSN low (start a frame)
+    digitalWriteFast(PIN_CSN, LOW);
+
+    // for each bit in buffer
+
+    for (int buffer_ctr = 0; buffer_ctr < ENCODER_BUFFER; buffer_ctr++)
+    {
+        // set clock high
+        digitalWriteFast(PIN_SCK, HIGH);
+        value <<= 1;
+        delayMicroseconds(1);
+        // set clock low
+        digitalWriteFast(PIN_SCK, LOW);
+        // now SCK has been pulsed the slave will have written a bit to MISO
+        slave_read_bit = digitalReadFast(PIN_MISO);
+        value |= slave_read_bit;
+
+        if (buffer_ctr > 0) {
+            parity += slave_read_bit;
+        }
+    }
+
+    
+
+    // set CSN high (end frame)
+    digitalWriteFast(PIN_CSN, HIGH);
+
+    // final parity check
+    bool final_parity =  ( (parity&1) != (value>>ENCODER_BUFFER_MAX_INDEX) );
+
+    // get the final result
+
+    value = (value & ENCODER_BUFFER_VALUE_MASK);
+
+    return final_parity;
+}
--- tracking/absolute-rotation-encoder-AS5147P/absolute-rotation-encoder-AS5147P.ino
+++ tracking/absolute-rotation-encoder-AS5147P/absolute-rotation-encoder-AS5147P.ino
@@ -1,9 +1,17 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "AS5147P/main.cpp"
 
+void setup() {
+  as5147p_setup();
 }
+
+uint16_t value;
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  auto parity = as5147p_get_raw_sensor_value(value);
+  Serial.print(value);
+  Serial.print("\t");
+  Serial.print(parity);
+  Serial.print("\n");
+  delayMicroseconds(1000); // 1000hz
 }
