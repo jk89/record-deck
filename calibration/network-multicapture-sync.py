10d10a532e323a24281204ea0a1f2fb29b9ac33f
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-08-01 15:41:36 +0100

    fixing copy pasta

--- calibration/network-multicapture-sync.py
+++ calibration/network-multicapture-sync.py
@@ -9,4 +9,4 @@ if len_argv == 2:
     network_sync_port = sys.argv[1]
     print((network_sync_port))
 else:
-    print(str(len_argv - 1) + " arguments provided. Expected 1 [tty source, stdout redirect, host, port]")
+    print(str(len_argv - 1) + " arguments provided. Expected 1 [port]")
