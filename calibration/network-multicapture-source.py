093ed06f319e655851efe6a439d962df84bbbdda
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-08-01 15:54:58 +0100

    draft for network-multicapture-sync just printing for now

--- calibration/network-multicapture-source.py
+++ calibration/network-multicapture-source.py
@@ -5,6 +5,19 @@
 # 4th 8132 [port]
 
 import sys
+import socket
+import json
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+def send_obj(obj):
+    str_obj = json.dumps(obj)
+    sock.sendto(str_obj.encode(), (UDP_IP, UDP_PORT))
+
 len_argv = len(sys.argv)
 
 if len_argv == 5:
@@ -12,6 +25,12 @@ if len_argv == 5:
     std_redirect = sys.argv[2]
     network_sync_host = sys.argv[3]
     network_sync_port = sys.argv[4]
+
+    UDP_IP = network_sync_host
+    UDP_PORT = network_sync_port
     # print((source, std_redirect, network_sync_host, network_sync_port))
 else:
     print(str(len_argv - 1) + " arguments provided. Expected 4 [tty source, stdout redirect, host, port]")
+    quit()
+
+    
--- calibration/network-multicapture-sync.py
+++ calibration/network-multicapture-sync.py
@@ -1,12 +1,27 @@
 # read args
 # 1st 8132 [port]
 
-
+import socket
 import sys
+import json
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5005
+
 len_argv = len(sys.argv)
 
 if len_argv == 2:
     network_sync_port = sys.argv[1]
+    UDP_PORT = int(network_sync_port)
     print((network_sync_port))
 else:
     print(str(len_argv - 1) + " arguments provided. Expected 1 [port]")
+    quit()
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((UDP_IP, UDP_PORT))
+
+while True:
+    message, address = server_socket.recvfrom(1024)
+    print(message)
