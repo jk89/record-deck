57005566389a9545d2a0fea94bf378fb2344ba46
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-08-04 13:04:39 +0100

    removing prints

--- calibration/network-multicapture-source.py
+++ calibration/network-multicapture-source.py
@@ -63,9 +63,9 @@ print("starting routine")
 
 with serial.Serial(SOURCE) as ser: # , 19200, timeout=1
     while True:    
-        print("decode line")
+        # print("decode line")
         line = str(ser.readline().decode('utf-8'))
-        print("line", line)
+        # print("line", line)
         if (line == "waiting\r\n"):
             send_start_cmd_teensy(ser)
             continue
@@ -73,7 +73,7 @@ with serial.Serial(SOURCE) as ser: # , 19200, timeout=1
         # print("line_split", line_split)
         time =int(line_split[0]) # time
         obj_to_send = {"time": time, "deviceId": SOURCE_DEVICE_ID, "line": line}
-        print("obj_to_send", obj_to_send)
+        print(obj_to_send)
         send_obj(obj_to_send)
         append_line_to_std_redirect(line)
 # send start byte
