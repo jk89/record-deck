66a541b1b1ec3089d3eac3faa8793210b072a72b
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-08-05 17:53:34 +0100

    working on arguments and tidy up of network-multicapture-source.js

--- calibration/network-multicapture-source.js
+++ calibration/network-multicapture-source.js
@@ -1,58 +1,60 @@
 const dgram = require('dgram');
-const client = dgram.createSocket('udp4');
-//client.send('Hello World!',0, 12, 12000, '127.0.0.1');
+const process = require('process');
+const { SerialPort, ReadlineParser } = require('serialport');
 
-let a = require('serialport');
-// console.log(a); process.exit();
+function process_args() {
+    if (process.argv.length !== 6) {
+        console.error("Expected 3 arguments: [source (e.g. '/dev/ttyACMP0'), network_sync_host (e.g. '192.168.0.26'), network_sync_port (e.g. 8132)]");
+        process.exit(1);
+    }
+    let [device_id, source, host, port] = process.argv.slice(2, 6);
 
-let { SerialPort, ReadlineParser } = require('serialport');
+    const device_id_int = parseInt(device_id);
+    if (isNaN(device_id_int)) {
+        console.error("Could not parse provided device_id to a number: ", device_id);
+        process.exit(1);
+    }
+    else {
+        device_id = device_id_int;
+    }
 
-async function getTeensy() {
-    // const ports = await SerialPort.list();
-    // const teensyPort = ports.find((port)=>port.path=="/dev/ttyACM0");
-    // return teensyPort;
+    const port_int = parseInt(port);
+    if (isNaN(port_int)) {
+        console.error("Could not parse provided port to a number: ", port);
+        process.exit(1);
+    }
+    else {
+        port = port_int;
+    }
+    return {device_id, source, host, port};
+}
+
+function get_teensy_serial_port(source) {
     return new SerialPort(
         {
-            path: "/dev/ttyACM0",
-            baudRate: 5000000,
-            // parser: new ReadlineParser()
+            path: source,
+            baudRate: 5000000
         }
     );
 }
 
+function main(source, network_sync_host, network_sync_port, device_id) {
+    const teensy_serial_port = get_teensy_serial_port(source);
+    const parser = teensy_serial_port.pipe(new ReadlineParser({ delimiter: '\n' }));
+    const client = dgram.createSocket('udp4');
 
-let i = 0;
-// 3984 2048 10s 40khz
-async function main() {
-    const teensySerialPort = await getTeensy();
-    
-
-    // setTimeout(()=>teensySerialPort.write("somejunktoget itstarted"), 3000);
-
-    const parser = teensySerialPort.pipe(new ReadlineParser({ delimiter: '\n' }));
     parser.on("data", (line) => {
-        
         const line_split = line.split("\t");
-        const network_obj = {"time": parseInt(line_split[0]), "deviceId": 0, line:line};
+        const network_obj = {"time": parseInt(line_split[0]), "deviceId": device_id, line:line};
         const network_str = JSON.stringify(network_obj);
-        // console.log(network_str);
-        client.send(network_str, 8132, '192.168.0.26');
+        client.send(network_str, network_sync_port, network_sync_host);
     });
 
-    teensySerialPort.write("somejunktoget itstarted");
+    teensy_serial_port.write("somejunktoget itstarted");
 
-    // console.log("spooling up");
-    /*teensySerialPort.on("data", (data) => {
-        // console.log(i, );
-        const line = data.toString();
-        const line_split = line.split("\t");
-        const network_obj = {"time": line_split[0], "deviceId": 0, line:line};
-        console.log(i, line.length);
-        // 
-        i++;
-    });*/
-
-    return teensySerialPort;
+    return teensy_serial_port;
 }
 
-main().then(console.log).catch(console.error);
+const args = process_args();
+// invoke main
+main(args.source, args.host, args.port, args.device_id);
