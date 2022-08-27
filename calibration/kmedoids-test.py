deb0fedb5cd246c31172530f3a2acd303236d5ec
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2022-08-27 01:02:18 +0100

    +

--- calibration/kmedoids-test.py
+++ calibration/kmedoids-test.py
@@ -8,14 +8,15 @@ sc = pyspark.SparkContext(master="spark://10.0.0.3:6060")
 sc.addPyFile("calibration/kmedoids2.py")
 spark = SparkSession(sc)
 
+# if nessesary: pip install pyclustering 
 from pyclustering.cluster import cluster_visualizer
 from pyclustering.utils import read_sample
 from pyclustering.samples.definitions import FCPS_SAMPLES
 from pyclustering.samples.definitions import SIMPLE_SAMPLES
 
 # use pyclustering datasets for a sanity check
-sample = read_sample(FCPS_SAMPLES.SAMPLE_GOLF_BALL) #SAMPLE_ENGY_TIME
-best_centroids, best_clusters = kmedoids2.fit(sc, sample, 20)
+sample = read_sample(FCPS_SAMPLES.SAMPLE_ENGY_TIME) #SAMPLE_ENGY_TIME
+best_centroids, best_clusters = kmedoids2.fit(sc, sample, 20) # SAMPLE_GOLF_BALL 4 8 20 (good checks), SAMPLE_ENGY_TIME 20 fun
 print(best_centroids)
 visualizer = cluster_visualizer()
 visualizer.append_clusters(best_clusters, sample)
