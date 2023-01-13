3423056bcfe2fb50e8f30eafee2b1c2cf7169fa4
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-01-13 14:06:31 +0000

    first fit with real data

--- calibration/analysis-test3.py
+++ calibration/analysis-test3.py
@@ -19,6 +19,10 @@ with open(file_in_json, "r") as fin:
 
 time_data, angle_data, a_neg_vn_data, b_neg_vn_data, c_neg_vn_data = list(map(lambda x:np.asarray(x), [time_data, angle_data, a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]))
 
+# modify angle_data to be in degrees
+encoder_value_to_angle = 360 / 2 ** 14
+angle_data = angle_data * encoder_value_to_angle
+
 print(list(map(lambda x: len(x), [time_data, angle_data, a_neg_vn_data, b_neg_vn_data, c_neg_vn_data])))
 print(list(map(lambda x: x[0], [time_data, angle_data, a_neg_vn_data, b_neg_vn_data, c_neg_vn_data])))
 print(list(map(lambda x: type(x[0]), [time_data, angle_data, a_neg_vn_data, b_neg_vn_data, c_neg_vn_data])))
@@ -26,26 +30,31 @@ print(list(map(lambda x: (x.shape), [time_data, angle_data, a_neg_vn_data, b_neg
 
 
 # define model function
-def model(angular_position, angular_displacement, phase_current_displacement, *fourier_coefficients):
-    phase_a_current = 0
-    phase_b_current = 0
-    phase_c_current = 0
-    for i, coefficient in enumerate(fourier_coefficients):
-        phase_a_current += coefficient * np.sin((i+1) * (angular_position + angular_displacement))
-        phase_b_current += coefficient * np.sin((i+1) * (angular_position + angular_displacement + phase_current_displacement))
-        phase_c_current += coefficient * np.sin((i+1) * (angular_position + angular_displacement + 2 * phase_current_displacement))
-    return phase_a_current, phase_b_current, phase_c_current
+def model(fourier_coefficients):
+    def fixed_param_model(angular_position, angular_displacement, phase_current_displacement):
+        phase_a_current = np.zeros((1,angular_position.shape[0]))# 0
+        phase_b_current = np.zeros((1,angular_position.shape[0]))# 0
+        phase_c_current = np.zeros((1,angular_position.shape[0])) # 0
+        for i, coefficient in enumerate(fourier_coefficients):
+            phase_a_current += coefficient * np.sin((i+1) * (angular_position + angular_displacement))
+            phase_b_current += coefficient * np.sin((i+1) * (angular_position + angular_displacement + phase_current_displacement))
+            phase_c_current += coefficient * np.sin((i+1) * (angular_position + angular_displacement + 2 * phase_current_displacement))
+        return np.asarray([phase_a_current, phase_b_current, phase_c_current]).ravel()
+    return fixed_param_model
+
+def helper_model():
+    pass
 
 # define Fourier series model function
 def fourier_model(angular_position, phase_current_displacement, *fourier_coefficients):
-    phase_a_current = 0
-    phase_b_current = 0
-    phase_c_current = 0
+    phase_a_current = np.zeros((1,angular_position.shape[0]))# 0# np.zeros((1,angular_position.shape[0]))
+    phase_b_current = np.zeros((1,angular_position.shape[0]))# 0
+    phase_c_current = np.zeros((1,angular_position.shape[0])) #0
     for i, coefficient in enumerate(fourier_coefficients):
         phase_a_current += coefficient * np.sin((i+1) * angular_position)
         phase_b_current += coefficient * np.sin((i+1) * (angular_position + phase_current_displacement))
         phase_c_current += coefficient * np.sin((i+1) * (angular_position + 2 * phase_current_displacement))
-    return phase_a_current, phase_b_current, phase_c_current
+    return np.asarray([phase_a_current, phase_b_current, phase_c_current]).ravel()
 
 """
 def f_1(x, p0, p1):
@@ -55,29 +64,51 @@ def f_2(x, p0, p1, p2):
     return p0 + p1*x + p2*x**2
 """
 
-phase_a_current, phase_b_current, phase_c_current = fourier_model(angle_data, np.asarray([a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]), *[1.0])
-print(list(map(lambda x: len(x), [phase_a_current, phase_b_current, phase_c_current])))
-print(np.asarray([a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]).shape)
-print(angle_data.shape)
-print(angle_data.reshape(1,angle_data.shape[0]).shape)
+#phase_a_current, phase_b_current, phase_c_current = fourier_model(angle_data, np.asarray([a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]), *[1.0])
+#print(list(map(lambda x: len(x), [phase_a_current, phase_b_current, phase_c_current])))
+#print(np.asarray([a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]).shape)
+#print(angle_data.shape)
+#print(angle_data.reshape(1,angle_data.shape[0]).shape)
+data_to_fit = np.asarray([a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]).ravel()
 
-curve_fit(fourier_model, angle_data, np.asarray([a_neg_vn_data, b_neg_vn_data, c_neg_vn_data]))
-
-"""
 # fit Fourier series model to data
-fourier_params, fourier_cov = curve_fit(fourier_model, angle_data, (a_neg_vn_data, b_neg_vn_data, c_neg_vn_data))
+initial_fourier_coefficients = (0,0,0,0,0,0,0,0,0,0)
+fourier_params, fourier_cov = curve_fit(fourier_model, angle_data, data_to_fit, p0=initial_fourier_coefficients)
 fourier_coefficients = fourier_params
 fourier_errors = np.sqrt(np.diag(fourier_cov))
 
+print("fourier_coefficients", fourier_coefficients)
+print("fourier_errors", fourier_errors)
+
 # fit the model to the data using the calculated Fourier coefficients
-params, cov = curve_fit(model, angle_data, (a_neg_vn_data, b_neg_vn_data, c_neg_vn_data), p0=[0, 0], args=fourier_coefficients)
+# angular_position, angular_displacement, phase_current_displacement, *fourier_coefficients
+
+params, cov = curve_fit(model(fourier_coefficients), angle_data, data_to_fit, [0, 0])
 errors = np.sqrt(np.diag(cov))
 
 # the optimal values for the parameters are the angular_displacement and phase_current_displacement
 angular_displacement = params[0]
 phase_current_displacement = params[1]
 
+def deg_to_rad(deg):
+    return deg * np.pi/180
+
+def rad_to_deg(rad):
+    return rad * 180/np.pi
+
 print(f'angular_displacement: {angular_displacement:.2f} +/- {errors[0]:.2f}')
 print(f'phase_current_displacement: {phase_current_displacement:.2f} +/- {errors[1]:.2f}') 
-"""
+
+print(f'phase_current_displacement: {rad_to_deg(phase_current_displacement):.2f} +/- {rad_to_deg(errors[1]):.2f}') 
+
+# angle_data [multi]
+# angular_displacement [fixed]
+# phase_current_displacement [fixed]
+# fourier_coeff [fixed]
+# data_to_fit [multi]
+# fitted_data
+fitted_data = model(fourier_coefficients)(angle_data, angular_displacement, phase_current_displacement)
+# print this data
+# scatter for data_to_fit
+# line for fitted_data
 print("TERM")
