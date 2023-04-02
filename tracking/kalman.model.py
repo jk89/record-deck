d67d2e839eb8eb249d8bb2477f0f9f2e2e6cdd90
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2023-04-02 14:43:42 +0100

    wokring on kalman

--- tracking/kalman.model.py
+++ tracking/kalman.model.py
@@ -33,6 +33,14 @@ init_Q = Matrix([
     [Pow(dt, 4)/24,Pow(dt, 3)/6,Pow(dt, 2)/2,dt]
     ])
 
+q11, q12, q13, q14, q21, q22, q23, q24, q31, q32, q33, q34, q41, q42, q43, q44 = symbols('q11 q12 q13 q14 q21 q22 q23 q24 q31 q32 q33 q34 q41 q42 q43 q44')
+Q = Matrix([
+    [q11, q12, q13, q14],
+    [q21, q22, q23, q24],
+    [q31, q32, q33, q34],
+    [q41, q42, q43, q44]
+])
+
 """ F_low_alpha_T
     ([
         [1.0, T  , t2Over2, math.pow(T,3)/6],
@@ -48,6 +56,14 @@ init_F = Matrix([
     [0.0, 0.0, 1.0, dt],
     [0.0, 0.0, 0.0, 1.0]
     ])
+
+f11, f12, f13, f14, f21, f22, f23, f24, f31, f32, f33, f34, f41, f42, f43, f44 = symbols('f11 f12 f13 f14 f21 f22 f23 f24 f31 f32 f33 f34 f41 f42 f43 f44')
+F = Matrix([
+    [f11, f12, f13, f14],
+    [f21, f22, f23, f24],
+    [f31, f32, f33, f34],
+    [f41, f42, f43, f44]
+])
 
 """
 initial p
@@ -66,6 +82,31 @@ init_P = Matrix([
     [0.0, (5*vj*Pow(dt, 2))/6, vj * dt, vj]
 ])
 
+p11, p12, p13, p14, p21, p22, p23, p24, p31, p32, p33, p34, p41, p42, p43, p44 = symbols('p11 p12 p13 p14 p21 p22 p23 p24 p31 p32 p33 p34 p41 p42 p43 p44')
+P = Matrix([
+    [p11, p12, p13, p14],
+    [p21, p22, p23, p24],
+    [p31, p32, p33, p34],
+    [p41, p42, p43, p44]
+])
+
+
+"""
+Measurement matrix
+H: Measurement matrix [1x4]
+"""
+
+H = Matrix([[1.0, 0.0, 0.0, 0.0]])
+
+
+"""
+Measurement error matrix
+ *  sigma_x: standard deviation of variable x (uncertainty in measurement x)
+ *  R: [[sigma_x^2]] [1x1]
+"""
+R = Matrix([vt])
+
+
 """
 state X = [x1,x2,x3,x4] = [pos, speed, acc, jerk]
 
@@ -82,7 +123,7 @@ take last known state X_{k} and project it ahead in time, one step forward using
  ignore w_{k} as it comes from the environment
 """
 
-X_kp1 = init_F * X_k
+X_kp1 = F * X_k
 
 
 """
@@ -90,3 +131,31 @@ X_kp1 = init_F * X_k
 Project the error covariance ahead:
  *  P_{k,k+1} = F_{k,k+1} * P_{-k,k} * F_{k,k+1}.T + Q_{k,k+1}
 """
+
+P_kp1 = F * P * F.transpose() + Q
+
+
+"""
+3)
+ *  Calculate uncertainty in estimate + uncertainty in measurement:
+ *  S = H*P_{k,k+1}*H.T + R
+"""
+
+S = H * P_kp1 * H.transpose() + R
+
+"""
+4)
+ *  Calculate uncertainty in estimate:
+ *  C = (P_{k,k+1}*H.T)
+ """
+
+C = P_kp1 * H.transpose()
+
+
+"""
+5)
+
+ *  Calculate Kalman Gain:  (S^-1 is the inverted S matrix, in the 1d case it is just a recropical of a scalar)
+ *  K = C * S^-1
+"""
+
